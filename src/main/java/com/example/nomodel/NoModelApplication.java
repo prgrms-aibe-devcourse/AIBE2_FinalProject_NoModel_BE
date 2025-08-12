3d2368d6e7d60064aa867a184c2814b16e902d53
INUK-ai <arheon2469@gmail.com>
2025-08-12 11:21:56 +0900

    setting: project setup

---
+++ src/main/java/com/example/nomodel/NoModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.nomodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nomodel/NoModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nomodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
