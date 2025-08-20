be870f9553cefefcfb6adbff8327c5c489d7a0d0
INUK-ai <arheon2469@gmail.com>
2025-08-20 14:55:29 +0900

    feat: Member domain setup - Member - Email, Password VO - Status, Role - 예외 처리(입력 오류)

--- src/main/java/com/example/nomodel/_core/exception/ErrorCode.java
+++ src/main/java/com/example/nomodel/_core/exception/ErrorCode.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR("ISE001", HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    INVALID_REQUEST("IRE001", HttpStatus.BAD_REQUEST, "Invalid request"),
+    MEMBER_NOT_FOUND("MNF001", HttpStatus.NOT_FOUND, "Member not found"),
+    MEMBER_ALREADY_EXISTS("MAE001", HttpStatus.CONFLICT, "Member already exists"),
+    EMAIL_ALREADY_EXISTS("EAE001", HttpStatus.CONFLICT, "Email already exists"),
     ;
 
     private final String errorCode;
