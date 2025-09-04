2698fc55ce21ea993613d1ba0f1d5549f6ed83db
Ssun-shine <hayonini@gmail.com>
2025-09-04 15:10:38 +0900

    feat: 리뷰 등록 리워드 로직 구현

--- src/main/java/com/example/nomodel/review/application/service/ReviewService.java
+++ src/main/java/com/example/nomodel/review/application/service/ReviewService.java
@@ -2,6 +2,10 @@ package com.example.nomodel.review.application.service;
 
 import com.example.nomodel._core.exception.ApplicationException;
 import com.example.nomodel._core.exception.ErrorCode;
+import com.example.nomodel.point.application.service.PointService;
+import com.example.nomodel.point.domain.model.*;
+import com.example.nomodel.point.domain.repository.MemberPointBalanceRepository;
+import com.example.nomodel.point.domain.repository.PointTransactionRepository;
 import com.example.nomodel.review.application.dto.request.ReviewRequest;
 import com.example.nomodel.review.application.dto.response.ReviewResponse;
 import com.example.nomodel.review.domain.model.ModelReview;
@@ -12,6 +16,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +25,10 @@ import java.util.stream.Collectors;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final PointService pointService;
+
+    private final MemberPointBalanceRepository pointBalanceRepository;
+    private final PointTransactionRepository transactionRepository;
 
     //리뷰 등록(중복 방지 후 저장, DTO 변환하여 반환)
     public ReviewResponse createReview(Long reviewerId, Long modelId, ReviewRequest request) {
@@ -28,14 +37,21 @@ public class ReviewService {
             throw new ApplicationException(ErrorCode.DUPLICATE_REVIEW);
         }
 
+        // 1. 리뷰 생성 및 저장
         ModelReview review = new ModelReview(
                 reviewerId,
                 modelId,
                 new Rating(request.getRating()),
                 request.getContent()
         );
+        reviewRepository.save(review);
 
-        return ReviewResponse.from(reviewRepository.save(review));
+        // 2. 포인트 적립 로직 추가 PointService로 위임
+        pointService.rewardForReview(reviewerId, review.getId());
+
+        // 3. 리뷰 응답 반환
+        return ReviewResponse.from(review);
+
     }
 
     //리뷰 조회(모델 ID로 리뷰들을 조회, 비어있으면 예외발생, 있으면 Dto 변환하여 반환)
