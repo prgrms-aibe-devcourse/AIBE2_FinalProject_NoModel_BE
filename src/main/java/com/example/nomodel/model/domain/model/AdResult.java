11d98bd2c7196907973f34c949610c55a86ca970
hyojin <35914102+hyojin52@users.noreply.github.com>
2025-09-08 11:42:55 +0900

    fix: index 추가, lazyLoading 추가

--- src/main/java/com/example/nomodel/model/domain/model/AdResult.java
+++ src/main/java/com/example/nomodel/model/domain/model/AdResult.java
@@ -11,15 +11,21 @@ import lombok.Setter;
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "ad_result_tb")
+@Table(
+  name = "ad_result_tb",
+  indexes = {
+    @Index(name = "idx_ad_result_created_at", columnList = "created_at"),
+    @Index(name = "idx_ad_result_model_id", columnList = "model_id")
+  }
+)
 public class AdResult extends BaseEntity {
   
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "ad_result_id")
   private Long id;
-  
-  @ManyToOne
+
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "model_id", nullable = false)
   private AIModel aiModel;
   
